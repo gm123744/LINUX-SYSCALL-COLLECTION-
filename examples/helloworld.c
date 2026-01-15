221a0e5adbffe1154ce104edbe2e136bdb42c1e2
GM11 <skatamer53@gmail.com>
2026-01-15 15:06:06 +0000

    Update helloworld.c

--- examples/helloworld.c
+++ examples/helloworld.c
@@ -2,10 +2,20 @@
 #include "lsc.h"
 
 int main() {
-    lsc_write write;
-    write.wtype = WRITE_STDOUT;
-    write.dataptr = "Hello, World!\n";
-    write.datalen = 14;  
-    lsc_stdout(&write);
+    //write 1st side of hello world
+    lsc_write hello; //call write struct 
+    hello.wtype = WRITE_STDOUT; //specify stdout type
+    hello.dataptr = "Hello,"; //data pointer to string
+    hello.datalen = 6;  //length of string
+    lsc_stdout(&hello); //call lsc_stdout function with pointer to struct
+
+    //second side of hello world
+    lsc_write world;
+    world.wtype = WRITE_STDOUT;
+    world.dataptr = "world\n";
+    world.datalen = 6;  
+    lsc_stdout(&world);
     return 0;
 }
+//compile and link with:
+//gcc -nostartfiles -nodefaultlibs helloworld.c lsc.s -o hello -Wa,--no-warn
